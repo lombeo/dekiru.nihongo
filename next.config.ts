7bf741fafad298cb36bd8142ab22417f8f2082f8
lom <102901885+lombeo@users.noreply.github.com>
2024-10-28 13:34:04 +0700

    update next.config

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   images: {
-    domains: ['scontent.fhan15-1.fna.fbcdn.net', 'lh3.google.com', 'lh3.googleusercontent.com', 'img.vietqr.io', 'drive.usercontent.google.com'], // Add the domain here
+    domains: ['scontent.fhan15-1.fna.fbcdn.net', 'lh3.google.com', 'lh3.googleusercontent.com', 'img.vietqr.io', 'drive.usercontent.google.com', 'drive.google.com'], // Add the domain here
   },
 };
 
